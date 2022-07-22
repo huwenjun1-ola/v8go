58fac675205a4d0554b2766ad8f399502425c41b
zhengkaikai <Zheng.Kaikai@olaola.chat>
2022-07-23 00:22:15 +0800

    dll目录修改

--- cgo_win.go
+++ cgo_win.go
@@ -7,7 +7,8 @@
 package v8go
 
 // #cgo CFLAGS:-I${SRCDIR}/ -w
-// #cgo LDFLAGS: -L${SRCDIR}/lib/ -lv8_export
+// #cgo LDFLAGS: -L${SRCDIR}/lib/win_x86_64 -lv8_export
 import "C"
 
 //windows 下使用动态库链接,需要直接链接,不需要编译v8_export
+import _ "gitee.com/hasika/v8go/lib/win_x86_64"
