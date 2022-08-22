bcf87c98699b24406024b39dec7b287bc044c1d8
zhengkaikai <Zheng.Kaikai@olaola.chat>
2022-08-22 16:51:50 +0800

    v8go ts-in-go接入

--- go.mod
+++ go.mod
@@ -3,15 +3,7 @@ module gitee.com/hasika/v8go
 go 1.15
 
 require (
-	github.com/BurntSushi/toml v1.2.0 // indirect
-	github.com/clbanning/mxj/v2 v2.5.6 // indirect
-	github.com/gogf/gf/v2 v2.1.2
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-runewidth v0.0.13 // indirect
-	github.com/rivo/uniseg v0.3.1 // indirect
-	go.opentelemetry.io/otel/sdk v1.8.0 // indirect
+	gitee.com/hasika/ts-in-go v0.0.0-20220822085056-ffbb09fbcd7a
 	golang.org/x/net v0.0.0-20220728211354-c7608f3a8462
-	golang.org/x/sys v0.0.0-20220731174439-a90be440212d // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 
 )
---
+++ wrapper/v8go_wrapper.go
@@ -0,0 +1,109 @@
+package wrapper
+
+import (
+	. "gitee.com/hasika/ts-in-go"
+	"gitee.com/hasika/v8go"
+)
+
+type V8JsEnv struct {
+	Iso *v8go.Isolate
+	Ctx *v8go.Context
+}
+
+func (v *V8JsEnv) Init(debugPort uint32, class IObjectTemplate, waitDebugger bool, debugEnable bool) {
+	globalClass := class.(*V8GoObjectTemplateWrapper).template
+	v.Ctx = v8go.NewContext(v.Iso, globalClass)
+	if debugEnable {
+		ins := v8go.NewClient(v.Iso, v.Ctx, debugPort)
+		if waitDebugger {
+			ins.WaitDebugger()
+		}
+	}
+}
+
+func (v *V8JsEnv) Dispose() {
+	v.Ctx.Close()
+	v.Iso.Dispose()
+}
+
+func (v *V8JsEnv) GetGlobal() IObject {
+	return v.Ctx.Global()
+}
+
+type V8GoFunction struct {
+	fun *v8go.Function
+}
+
+func (v V8GoFunction) Call(recv IValue, args ...IValue) (IValue, error) {
+	v8args := make([]v8go.Valuer, len(args))
+	for i, arg := range args {
+		v8args[i] = arg.(v8go.Valuer)
+	}
+	return v.fun.Call(recv.(v8go.Valuer), v8args...)
+}
+
+type V8GoFunctionTemplateWrapper struct {
+	template *v8go.FunctionTemplate
+	env      *V8JsEnv
+}
+
+func (v *V8GoFunctionTemplateWrapper) Set(key string, value interface{}, constraints ...PropertyConstraint) error {
+	attributes := make([]v8go.PropertyAttribute, len(constraints))
+	for i, constraint := range constraints {
+		attributes[i] = v8go.PropertyAttribute(constraint)
+	}
+	return v.template.Set(key, value, attributes...)
+}
+
+func (v *V8GoFunctionTemplateWrapper) GetFunction() IFunction {
+	return &V8GoFunction{fun: v.template.GetFunction(v.env.Ctx)}
+}
+
+type V8GoObjectTemplateWrapper struct {
+	env      *V8JsEnv
+	template *v8go.ObjectTemplate
+}
+
+func (v *V8GoObjectTemplateWrapper) Set(key string, value interface{}, constraints ...PropertyConstraint) error {
+	attributes := make([]v8go.PropertyAttribute, len(constraints))
+	for i, constraint := range constraints {
+		attributes[i] = v8go.PropertyAttribute(constraint)
+	}
+	return v.template.Set(key, value, attributes...)
+}
+
+func (v *V8GoObjectTemplateWrapper) NewInstance() (IObject, error) {
+	return v.template.NewInstance(v.env.Ctx)
+}
+
+func (v *V8JsEnv) NewFunctionTemplate(call GoFunCallBack) IFunctionTemplate {
+	v8Template := v8go.NewFunctionTemplate(v.Iso, func(info *v8go.FunctionCallbackInfo) *v8go.Value {
+		args := make([]IObject, len(info.Args()))
+		for i, value := range info.Args() {
+			args[i] = value.Object()
+		}
+		ret := call(&CommonFunctionCallbackInfo{
+			This: info.This(),
+			Args: args,
+		})
+		return ret.(*v8go.Value)
+	})
+	return &V8GoFunctionTemplateWrapper{
+		template: v8Template,
+	}
+}
+
+func (v *V8JsEnv) NewObjectTemplate() IObjectTemplate {
+	v8GoObjectTemplate := v8go.NewObjectTemplate(v.Iso)
+	return &V8GoObjectTemplateWrapper{
+		template: v8GoObjectTemplate,
+	}
+}
+
+func (v *V8JsEnv) NewValue(goValue interface{}) (IValue, error) {
+	return v8go.NewValue(v.Iso, goValue)
+}
+
+func (v *V8JsEnv) RunScript(script string, fullPath string) (IValue, error) {
+	return v.Ctx.RunScript(script, fullPath)
+}
