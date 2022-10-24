487cb042fb73937a19b59dcf40721cfb9c9d473f
zhengkaikai <Zheng.Kaikai@olaola.chat>
2022-10-24 15:06:03 +0800

    Revert "修改编译方式"
    This reverts commit 207ae4e3315e6edacf6ce37bad27fe31841f00b4.

--- v8_export/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -e CMakeCache.txt ]; then
-  rm CMakeCache.txt
-fi
-mkdir -p build_cmake
-cmake -S ./ -B ./build_cmake
-cd build_cmake
-make
--- v8_export/build_copy_darwin_x86_64.sh
+++
@@ -1,5 +0,0 @@
-./build.sh
-cd ./build_cmake
-cp ./libv8_export* ../../lib/darwin_x86_64/
-cd ../
-rm -rf ./build_cmake
