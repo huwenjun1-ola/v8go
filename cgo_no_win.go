18d3724b7a7144a844ea40fc2051162bb438bc11
zhengkaikai <Zheng.Kaikai@olaola.chat>
2022-11-04 11:12:45 +0800

    Revert "c++ 1y"
    This reverts commit e6b119c1b8d383d2ecd1ef6405f62e4125ca1912.

--- cgo_no_win.go
+++ cgo_no_win.go
@@ -3,7 +3,7 @@
 
 package v8go
 
-// #cgo CXXFLAGS:-DCOMPILE_EXPORT -fpermissive -fno-rtti -fpic -std=c++1y -DV8_COMPRESS_POINTERS -DV8_31BIT_SMIS_ON_64BIT_ARCH -I${SRCDIR}/deps/include -I${SRCDIR}/ -w
+// #cgo CXXFLAGS:-DCOMPILE_EXPORT -fpermissive -fno-rtti -fpic -std=c++14 -DV8_COMPRESS_POINTERS -DV8_31BIT_SMIS_ON_64BIT_ARCH -I${SRCDIR}/deps/include -I${SRCDIR}/ -w
 // #cgo CFLAGS:-DCOMPILE_EXPORT -I${SRCDIR}/deps/include -I${SRCDIR}/ -w
 // #cgo LDFLAGS: -pthread -lv8
 // #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/lib/darwin_x86_64
