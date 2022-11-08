d35e505697b94d79dec1fbbe17866b821a59e330
zhengkaikai <Zheng.Kaikai@olaola.chat>
2022-11-08 12:15:40 +0800

    头文件挪走

--- cgo_no_win.go
+++ cgo_no_win.go
@@ -11,8 +11,8 @@ package v8go
 //// #cgo linux LDFLAGS: -L${SRCDIR}/lib/linux_x86_64 -ldl
 //import "C"
 
-// #cgo CFLAGS:-I${SRCDIR}/ -w -stdlib=libc++
-// #cgo CXXFLAGS:-I${SRCDIR}/ -w -stdlib=libc++
+// #cgo CFLAGS:-I${SRCDIR}/ -I${SRCDIR}/v8_export/ -w -stdlib=libc++
+// #cgo CXXFLAGS:-I${SRCDIR}/ -I${SRCDIR}/v8_export/ -w -stdlib=libc++
 // #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/lib/darwin_arm64 -lv8_export -lv8 -lv8_libbase -lv8_libplatform -latomic -ldl -lpthread -lrt -lc++ -lc++abi -lm
 // #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/lib/linux_x86_64 -lv8_export -lv8 -lv8_libbase -lv8_libplatform -latomic -ldl -lpthread -lrt -lc++ -lc++abi -lm
 import "C"
