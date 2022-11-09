df2b836ec3244b0201b5954e8002f777a25d1941
zhengkaikai <Zheng.Kaikai@olaola.chat>
2022-11-09 09:55:34 +0800

    更新版本

--- cgo_no_win.go
+++ cgo_no_win.go
@@ -13,7 +13,7 @@ package v8go
 
 // #cgo CFLAGS:-I${SRCDIR}/ -I${SRCDIR}/v8_export/ -w -stdlib=libc++
 // #cgo CXXFLAGS:-I${SRCDIR}/ -I${SRCDIR}/v8_export/ -w -stdlib=libc++
-// #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/lib/darwin_arm64 -lv8_export -lv8_monolith -lv8_libbase -lv8_libplatform -latomic -ldl -lpthread -lrt -lc++ -lc++abi -lm
+// #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/lib/darwin_arm64 -lv8_export -lv8_monolith -lv8_libbase -lv8_libplatform  -ldl -lpthread -lc++ -lc++abi -lm
 // #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/lib/linux_x86_64 -lv8_export -lv8_monolith -lv8_libbase -lv8_libplatform -latomic -ldl -lpthread -lrt -lc++ -lc++abi -lm
 import "C"
 import (
