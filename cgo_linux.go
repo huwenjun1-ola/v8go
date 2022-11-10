77cd77a08f00ab30bc7a229b6594e6cdc47f5ab6
zhenng <Zheng.Kaikai@olaola.chat>
2022-11-11 00:43:39 +0800

    9.2-linux

--- cgo_linux.go
+++ cgo_linux.go
@@ -4,7 +4,7 @@
 package v8go
 
 // #cgo CFLAGS:-I${SRCDIR}/ -I${SRCDIR}/v8_export/ -w -stdlib=libc++
-// #cgo CXXFLAGS:-I${SRCDIR}/ -I${SRCDIR}/v8_export/ -w -stdlib=libc++
+// #cgo CXXFLAGS:-I${SRCDIR}/ -I${SRCDIR}/v8_export/ -w -stdlib=libc++ -std=c++11
 // #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/lib/linux_x86_64 -lv8_export -lv8_monolith -lv8_libbase -lv8_libplatform -latomic -ldl -lpthread -lrt -lc++ -lc++abi -lm
 import "C"
 import (
--- v8_export_darwin/build_copy_darwin_arm64.sh
+++
@@ -1,5 +0,0 @@
-./build.sh
-cd ./build_cmake
-cp ./libv8_export* ../../lib/darwin_arm64/
-cd ../
-rm -rf ./build_cmake
