b27075be8a8aef05ff4b9ccfe9fa5c506b72843c
zhengkaikai <Zheng.Kaikai@olaola.chat>
2022-11-11 15:20:21 +0800

    新增darwin amd64

---
+++ lib/darwin_amd64/vendor.go
@@ -0,0 +1,3 @@
+// Package amd_arm64 is required to provide support for vendoring modules
+// DO NOT REMOVE
+package darwin_amd64
---
+++ v8_export_darwin/build_copy_darwin_amd64.sh
@@ -0,0 +1,5 @@
+./build.sh
+cd ./build_cmake
+cp ./libv8_export* ../../lib/darwin_amd64/
+cd ../
+rm -rf ./build_cmake
