9452151483b2516d5ff8c92592e3a288c7d965e9
zhenng <Zheng.Kaikai@olaola.chat>
2022-11-12 09:11:39 +0800

    gcc-9.2

--- cgo_linux.go
+++ cgo_linux.go
@@ -3,9 +3,9 @@
 
 package v8go
 
-// #cgo CFLAGS:-I${SRCDIR}/ -I${SRCDIR}/v8_export/ -w -stdlib=libc++
-// #cgo CXXFLAGS:-I${SRCDIR}/ -I${SRCDIR}/v8_export/ -w -stdlib=libc++ -std=c++11
-// #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/lib/linux_x86_64 -lv8_export -lv8_monolith -lv8_libbase -lv8_libplatform -latomic -ldl -lpthread -lrt -lc++ -lc++abi -lm
+// #cgo CFLAGS:-I${SRCDIR}/ -I${SRCDIR}/v8_export/ -w
+// #cgo CXXFLAGS:-I${SRCDIR}/ -I${SRCDIR}/v8_export/ -w
+// #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/lib/linux_x86_64 -lv8_export -lv8_monolith -lv8_libbase -lv8_libplatform -latomic -ldl -lstdc++ -lpthread -lrt -lm
 import "C"
 import (
 	_ "gitee.com/hasika/v8go/lib/linux_x86_64"
